575113c10110ba1d2573c309070a742d65e4386a
ravan-Aliyev <eliyevrevan18@outlook.com>
2024-01-22 04:55:49 -0800

    find last digit

---
+++ python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+last_num = abs(number) % 10
+if (number < 0):
+    last_num *= -1
+print(f"Last digit of {number} is {last_num} and is", end=" ")
+if (last_num > 5):
+    print("greater than 5")
+elif (last_num == 0):
+    print("is 0")
+else:
+    print("less than 6 and not 0")
