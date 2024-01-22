333892250a194cacf841911d3521c4d0a7a08d4f
ravan-Aliyev <eliyevrevan18@outlook.com>
2024-01-22 04:57:25 -0800

    find last digit

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -8,6 +8,6 @@ print(f"Last digit of {number} is {last_num} and is", end=" ")
 if (last_num > 5):
     print("greater than 5")
 elif (last_num == 0):
-    print("is 0")
+    print("0")
 else:
     print("less than 6 and not 0")
