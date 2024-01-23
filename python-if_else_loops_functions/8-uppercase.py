3b4302303dd4c795dea3cffa3736af7ceafb490c
ravan-Aliyev <eliyevrevan18@outlook.com>
2024-01-23 01:05:57 -0800

    uppercase

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+def uppercase(str):
+    str_cpy = ""
+    for i in range(len(str)):
+        if ord('a') <= ord(str[i]) <= ord('z'):
+            str_cpy += chr(ord(str[i]) - 32)
+            continue
+        str_cpy += str[i]
+    print(str_cpy)
