9c5b3d4c08930ed3f7a28d10675c0b6720fc4911
ravan-Aliyev <eliyevrevan18@outlook.com>
2024-01-23 22:19:09 -0800

    add tuples

---
+++ python-data_structures/7-add_tuple.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    if tuple_a == ():
+        tuple_a = (0, 0)
+    else:
+        if len(tuple_a) == 1:
+            tuple_a = (tuple_a[0], 0)
+
+    if tuple_b == ():
+        tuple_b = (0, 0)
+    else:
+        if len(tuple_b) == 1:
+            tuple_b = (tuple_b[0], 0)
+
+    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
