9acb174d7d4836c71e444fb95014c4750c41041c
ravan-Aliyev <eliyevrevan18@outlook.com>
2024-01-29 18:47:44 +0400

    coordinates of square

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -50,9 +50,9 @@ class Square:
         if self.__size == 0:
             print("")
             return
+
         [print("") for i in range(0, self.__position[1])]
         for i in range(self.__size):
-            print("{:{}}".format("", self.__position[0]), end="")
-            for j in range(self.__size):
-                print("#", end="")
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
             print("")
