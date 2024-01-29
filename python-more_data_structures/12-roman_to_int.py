ee0eac647660d69e89922fecd99e6b2e096a8372
ravan-Aliyev <eliyevrevan18@outlook.com>
2024-01-29 13:57:35 +0400

    rmoan to integer

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    roman_numerals = {'I': 1, 'V': 5, 'X': 10,
+                      'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    decimal = 0
+    prev = 0
+    for i in reversed(roman_string):
+        value = roman_numerals[i]
+
+        if value < prev:
+            decimal -= value
+        else:
+            decimal += value
+        prev = value
+    return decimal
