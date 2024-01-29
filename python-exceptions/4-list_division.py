befa6ffa5ea9351da3ff1302e55484d59c15fc1c
ravan-Aliyev <eliyevrevan18@outlook.com>
2024-01-29 15:29:40 +0400

    safe list division

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -6,7 +6,7 @@ def list_division(my_list_1, my_list_2, list_length):
             result = my_list_1[i] / my_list_2[i]
         except ZeroDivisionError:
             print("division by 0")
-        except TypeError:
+        except (ValueError, TypeError):
             print("wrong type")
         except IndexError:
             print("out of range")
