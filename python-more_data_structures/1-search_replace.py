28a2955ce4a4b0824672866d2d1083dc9cd91753
ravan-Aliyev <eliyevrevan18@outlook.com>
2024-01-29 11:12:52 +0400

    search and replase

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,6 +1,7 @@
-#!/usr/bin/python3i
+#!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    copy = my_list[:]
-    index = copy.index(search)
-    copy[index] = replace
-    return copy
+    new_list = my_list[:]
+    for i in range(len(new_list)):
+        if new_list[i] == search:
+            new_list[i] = replace
+    return (new_list)
