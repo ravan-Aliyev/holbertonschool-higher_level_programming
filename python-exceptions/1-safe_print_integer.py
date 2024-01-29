acf8f42b2566e5259bac4648c19f3ff691fe746e
ravan-Aliyev <eliyevrevan18@outlook.com>
2024-01-29 14:40:18 +0400

    safe print integer

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -2,6 +2,6 @@
 def safe_print_integer(value):
     try:
         print("{:d}".format(int(value)))
-    except ValueError:
+    except (ValueError, TypeError):
         return False
     return True
