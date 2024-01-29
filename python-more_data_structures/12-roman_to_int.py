2bab27d4a9072b033d0d6781487bd9df66882bd1
ravan-Aliyev <eliyevrevan18@outlook.com>
2024-01-29 14:06:16 +0400

    roman to decimal

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -4,6 +4,8 @@ def roman_to_int(roman_string):
                       'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     decimal = 0
     prev = 0
+    if (not isinstance(roman_string, str) or roman_string is None):
+        return None
     for i in reversed(roman_string):
         value = roman_numerals[i]
 
