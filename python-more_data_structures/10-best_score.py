9b2b64784f9a64977cf6596f5c577c69abfe8b1c
ravan-Aliyev <eliyevrevan18@outlook.com>
2024-01-29 12:58:28 +0400

    best key

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None:
+    if a_dictionary is None or len(a_dictionary) == 0:
         return None
     sorted_key = sorted(a_dictionary.items(), key=lambda x: x[1])
     return sorted_key[-1][0]
