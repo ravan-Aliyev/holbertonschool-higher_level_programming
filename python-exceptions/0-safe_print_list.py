2a98fab87520a1b39e49710c232e0666f24ba236
ravan-Aliyev <eliyevrevan18@outlook.com>
2024-01-29 14:30:14 +0400

    safe list print

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -3,9 +3,9 @@ def safe_print_list(my_list=[], x=0):
     ret = 0
     for i in range(x):
         try:
-            print(my_list[i], end="")
-            rt += 1
+            print("{}".format(my_list[i]), end="")
+            ret += 1
         except IndexError:
             break
     print("")
-    return ret
+    return (ret)
