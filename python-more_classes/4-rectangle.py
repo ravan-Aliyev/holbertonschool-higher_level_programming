04ccf4f950800180b35ead53b2201e640a223d7b
ravan-Aliyev <eliyevrevan18@outlook.com>
2024-02-05 13:12:22 +0400

    repr function

---
+++ python-more_classes/4-rectangle.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""Create new class"""
+
+
+class Rectangle:
+    """New rectangle class"""
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+
+        if value < 0:
+            raise ValueError("width must be >= 0")
+
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+
+        if value < 0:
+            raise ValueError("height must be >= 0")
+
+        self.__height = value
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        if self.width == 0 or self.height == 0:
+            return 0
+        return 2 * (self.height + self.width)
+
+    def __str__(self) -> str:
+        result = ""
+        for _ in range(self.height):
+            result += "#" * self.__width + '\n'
+        return result.rstrip("\n")
+
+    def __repr__(self) -> str:
+        return f"Rectangle({self.width}, {self.height})"
