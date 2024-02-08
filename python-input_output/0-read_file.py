6aa4360628f98ff5f9fef9df3595ba8bf8e6088d
ravan-Aliyev <eliyevrevan18@outlook.com>
2024-02-08 16:01:11 +0400

    hot fix 0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -6,4 +6,4 @@ def read_file(filename=""):
     """Read data from filename"""
     with open(filename, encoding="utf-8") as file:
         read_data = file.read()
-    print(read_data)
+        print(read_data)
