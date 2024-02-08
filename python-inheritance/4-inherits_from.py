6c8312dc888eeb4284b2dcb224dcd0a6f3e77ac6
ravan-Aliyev <eliyevrevan18@outlook.com>
2024-02-08 15:53:58 +0400

    fixing

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -4,6 +4,6 @@
 
 def inherits_from(obj, a_class):
     """Check type"""
-    if issubclass(type(obj), a_class) and type(obj) != a_class:
+    if issubclass(type(obj), a_class) and type(obj) is not a_class:
         return True
     return False
