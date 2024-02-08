b8379248f0bb42b93d83db0ba87e380bf80f1576
ravan-Aliyev <eliyevrevan18@outlook.com>
2024-02-08 17:01:15 +0400

    reload form json

---
+++ python-input_output/11-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Student class"""
+
+
+class Student:
+    """Student class"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        new_dict = {}
+        for i in self.__dict__:
+            if i in attrs:
+                new_dict[i] = self.__dict__[i]
+
+        return new_dict
+
+    def reload_from_json(self, json):
+        self.first_name = json["first_name"]
+        self.last_name = json["last_name"]
+        self.age = json["age"]
