a32dd5218a35893e16e1138aa14a00c9ffa28b13
ravan-Aliyev <eliyevrevan18@outlook.com>
2024-02-08 16:48:13 +0400

    to json with filter

---
+++ python-input_output/10-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Student class"""
+
+
+class Student:
+    """Student class"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        
+        new_dict = {}
+        for i in self.__dict__:
+            if i in attrs:
+                new_dict[i] = self.__dict__[i]
+
+        return new_dict
