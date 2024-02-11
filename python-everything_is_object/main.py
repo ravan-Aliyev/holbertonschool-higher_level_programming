2283d2e46617f22cc068467b5f721f89c7e73a96
ravan-Aliyev <eliyevrevan18@outlook.com>
2024-02-11 18:44:43 +0400

    pascal triangle

--- python-everything_is_object/main.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-a = [1, 2, 3, 4]
-print(id(a))
-a += [5]
-print(id(a))
--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -4,15 +4,21 @@
 
 def pascal_triangle(n):
     """Pascal triangle"""
-    if n <= 0:
-        return []
-    arr = []
-    for i in range(1, n):
-        num = [j + 1 for j in range(i)]
-        num[-1] = num[0]
-        # num[i - 1] = num[(i - 1) + (i - 1)]
-        if (i > 1):
-            num[1] = num[1]
-        arr.append(num)
-    print(arr)
-    return [[1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1]]
+    matrix = []
+    prev = []
+
+    for i in range(n):
+        res_list = []
+        p1 = -1
+        p2 = 0
+        for j in range(len(prev) + 1):
+            if p1 == -1 or p2 == len(prev):
+                res_list += [1]
+            else:
+                res_list += [prev[p1] + prev[p2]]
+            p1 += 1
+            p2 += 1
+        matrix.append(res_list)
+        prev = res_list[:]
+
+    return matrix
