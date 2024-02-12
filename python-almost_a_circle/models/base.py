e4768818af228361ad4c17545fa7c0ad16483cc0
ravan-Aliyev <eliyevrevan18@outlook.com>
2024-02-12 15:57:18 +0400

    json to file

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Base class"""
 import json
+import os
 
 
 class Base:
@@ -19,3 +20,18 @@ class Base:
         if list_dictionaries is None:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        arr = []
+        file_path = f"{cls.__name__}.json"
+        for cl in list_objs:
+            class_dict = cl.to_dictionary()
+            arr.append(class_dict)
+
+        if os.path.exists(file_path):
+            with open(f"{cls.__name__}.json", "a") as f:
+                f.write(cls.to_json_string(arr))
+        else:
+            with open(f"{cls.__name__}.json", "w") as f:
+                f.write(cls.to_json_string(arr))
