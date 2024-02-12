dfbcb824e24ef7694c175ce382e757a3d68ba5fd
ravan-Aliyev <eliyevrevan18@outlook.com>
2024-02-12 12:54:07 +0400

    rectangle modify

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -19,10 +19,7 @@ class Rectangle(Base):
 
     @width.setter
     def width(self, value):
-        if type(value) != int:
-            raise TypeError("width must be an integer.")
-        if (value <= 0):
-            raise ValueError("width must be > 0.")
+        self.__value_dimansion(value, "width")
         self.__width = value
 
     @property
@@ -31,10 +28,7 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, value):
-        if type(value) != int:
-            raise TypeError("height must be an integer.")
-        if (value <= 0):
-            raise ValueError("height must be > 0.")
+        self.__value_dimansion(value, "height")
         self.__height = value
 
     @property
@@ -43,10 +37,7 @@ class Rectangle(Base):
 
     @x.setter
     def x(self, value):
-        if type(value) != int:
-            raise TypeError("x must be an integer.")
-        if (value < 0):
-            raise ValueError("x must be >= 0.")
+        self.__value_dimansion(value, "x")
         self.__x = value
 
     @property
@@ -55,8 +46,13 @@ class Rectangle(Base):
 
     @y.setter
     def y(self, value):
-        if type(value) != int:
-            raise TypeError("y must be an integer.")
-        if (value <= 0):
-            raise ValueError("y must be >= 0.")
+        self.__value_dimansion(value, "y")
         self.__y = value
+
+    def __value_dimansion(self, value, name):
+        if type(value) != int:
+            raise TypeError(f"{name} must be an integer.")
+        if (name[0] == 'x' or name[0] == 'y') and value < 0:
+            raise ValueError(f"{name} must be >= 0.")
+        elif (name[0] != 'x' and name[0] != 'y') and value <= 0:
+            raise ValueError(f"{name} must be > 0.")
