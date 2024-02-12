34750df99751093c7ac1769ec38352295925d95c
ravan-Aliyev <eliyevrevan18@outlook.com>
2024-02-12 14:44:16 +0400

    square update method

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -23,6 +23,33 @@ class Square(Rectangle):
         self.__width = value
         self.__height = value
 
+    def update(self, *args, **kwargs):
+        if args and len(args) != 0:
+            a = 0
+            for arg in args:
+                if a == 0:
+                    if arg is None:
+                        self.__init__(self.width, self.height, self.x, self.y)
+                    else:
+                        self.id = arg
+                elif a == 1:
+                    self.width = arg
+                elif a == 2:
+                    self.x = arg
+                elif a == 3:
+                    self.y = arg
+                a += 1
+        elif kwargs and len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if key == "id":
+                    self.id = value
+                elif key == "size":
+                    self.width = value
+                elif key == "x":
+                    self.x = value
+                elif key == "y":
+                    self.y = value
+
     def __str__(self) -> str:
         return f"[Square] ({self.id}) {self.x}/{self.y}\
  - {self.width}"
