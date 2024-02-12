01f501649f1f4905390908c3b87b7fe887a81c22
ravan-Aliyev <eliyevrevan18@outlook.com>
2024-02-12 16:01:09 +0400

    json to file

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -23,15 +23,11 @@ class Base:
 
     @classmethod
     def save_to_file(cls, list_objs):
-        arr = []
-        file_path = f"{cls.__name__}.json"
-        for cl in list_objs:
-            class_dict = cl.to_dictionary()
-            arr.append(class_dict)
-
-        if os.path.exists(file_path):
-            with open(f"{cls.__name__}.json", "a") as f:
-                f.write(cls.to_json_string(arr))
-        else:
-            with open(f"{cls.__name__}.json", "w") as f:
-                f.write(cls.to_json_string(arr))
+        """Saves JSON str to file"""
+        filename = f"{cls.__name__}.json"
+        with open(filename, "w") as fd:
+            if list_objs is None:
+                fd.write("[]")
+            else:
+                list_dicts = [obj.to_dictionary() for obj in list_objs]
+                fd.write(Base.to_json_string(list_dicts))
