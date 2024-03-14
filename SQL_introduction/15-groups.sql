a55715bb4b335c4d1440217c02a6564a4b4bbcc1
ravan-Aliyev <eliyevrevan18@outlook.com>
2024-03-14 17:28:57 +0400

    number by score

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- Number by score
-SELECT score, COUNT(*) AS number FROM second_table GROUP BY number ORDER BY score DESC;
+SELECT score, COUNT(*) AS number FROM second_table GROUP BY score ORDER BY number DESC;
