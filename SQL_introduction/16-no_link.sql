7c2d32acd9b18fe2052d930e8994fcb2fb136c36
ravan-Aliyev <eliyevrevan18@outlook.com>
2024-03-14 17:33:17 +0400

    heisnberg

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- Say my name = "Heisnberg"
-SELECT score, name FROM second_table WHERE name != '' ORDER BY score DESC THEN BY name;
+SELECT score, name FROM second_table WHERE name != '' ORDER BY score DESC;
