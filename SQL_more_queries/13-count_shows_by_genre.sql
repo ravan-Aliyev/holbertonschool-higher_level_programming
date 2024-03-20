f46703f7333fb6954920320309ca6f018376780f
ravan-Aliyev <eliyevrevan18@outlook.com>
2024-03-20 15:23:01 +0400

    number of genres

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,2 +1,2 @@
 -- Number of genres
-SELECT tv_genres.name AS genre, COUNT(*) AS number_of_shows FROM tv_genres LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id GROUP BY tv_genres.name ORDER BY COUNT(*) DESC;
+SELECT tv_genres.name AS genre, COUNT(*) AS number_of_shows FROM tv_genres LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id WHERE tv_show_genres.genre_id IS NOT NULL GROUP BY tv_genres.name ORDER BY COUNT(*) DESC;
