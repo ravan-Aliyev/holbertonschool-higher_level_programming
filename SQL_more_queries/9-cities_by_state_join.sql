726bab6b007a2af93e59b1bdb7257a6ade149436
ravan-Aliyev <eliyevrevan18@outlook.com>
2024-03-20 14:35:26 +0400

    first join

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,2 @@
 -- Cities in states with join
-SELECT id, name, name FROM cities LEFT JOIN states ON cities.state_id = states.id;
+SELECT cities.id AS id, cities.name AS name, states.name AS name FROM cities LEFT JOIN states ON cities.state_id = states.id;
