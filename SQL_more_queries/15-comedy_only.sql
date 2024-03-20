0d6cd235e6aba5f1177d6bca628db97ae0e553b8
ravan-Aliyev <eliyevrevan18@outlook.com>
2024-03-20 15:38:20 +0400

    only title

---
+++ SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,3 @@
+-- My genres
+use hbtn_0d_tvshows;
+SELECT tv_shows.title AS title FROM tv_genres LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id LEFT JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id WHERE tv_genres.name = 'Comedy' ORDER BY tv_shows.title;
