29fa61970b8c037e817c32e7a4ef869e4c8c1881
ravan-Aliyev <eliyevrevan18@outlook.com>
2024-03-21 11:18:32 +0400

    filter states

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Connectio to database"""
+
+
+if __name__ == "__main__":
+    import sys
+    import MySQLdb
+    connection = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3]
+            )
+
+    cursor = connection.cursor()
+    cursor.execute("select * from states where 'N' in (select name from states)")
+
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    connection.close()
