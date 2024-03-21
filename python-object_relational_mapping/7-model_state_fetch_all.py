03bbe2a940bba2fbe497902f14232a7b43f5b8a0
ravan-Aliyev <eliyevrevan18@outlook.com>
2024-03-21 13:10:21 +0400

    all state via sqlalchemy

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -8,7 +8,7 @@ from sqlalchemy import (create_engine)
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}\
-                           @localhost/{}'.format(
+                           @localhost:3306/{}'.format(
                                sys.argv[1],
                                sys.argv[2], sys.argv[3]))
 
