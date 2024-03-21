ef12c354ff9ab620efc27af125749a3bc286c322
ravan-Aliyev <eliyevrevan18@outlook.com>
2024-03-21 11:33:06 +0400

    filter by input

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Connectio to database"""
+
+
+if __name__ == "__main__":
+    import sys
+    import MySQLdb
+    connection = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3]
+            )
+
+    search = sys.argv[4].strip('\'')
+    cursor = connection.cursor()
+    cursor.execute(f"select * from states\
+                   where name = {search}")
+
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    connection.close()
