7f340ad4b17379410338200d6574e4217b2d4eb1
ravan-Aliyev <eliyevrevan18@outlook.com>
2024-05-26 19:23:39 +0400

    change header when click

---
+++ javascript-dom_manipulation/1-script.js
@@ -0,0 +1,6 @@
+let header = document.querySelector('header');
+let red_header = document.querySelector('#red_header');
+
+red_header.addEventListener("click", () => {
+        header.style.color = '#FF0000';
+})
